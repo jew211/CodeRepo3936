d5a035c7d4057034716ec3dee5bbbeca6e563a00
Ethan <ethanrgreen2004@gmail.com>
2021-01-16 14:04:58 -0600

    Added a first test of trajectory tracking, following 5190

---
+++ Code_2021/Testing/First trajectory/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.subsystems.Drivetrain;
+
+import java.util.Arrays;
+
+public class RobotContainer {
+
+     private Drivetrain drive = new Drivetrain();
+     public Command getAutonomousCommand(){
+          TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(2) , Units.feetToMeters(2));
+          config.setKinematics(drive.getKinematics());
+
+          Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+                  Arrays.asList(new Pose2d(), new Pose2d(1.0, 0 , new Rotation2d())));
+
+          RamseteCommand command = new RamseteCommand(trajectory, drive::getPose, new RamseteController(2, 0.7), drive.getFeedforward(), drive.getKinematics(), drive::GetSpeeds,
+                  drive.getLeftPIDController(), drive.getRightPIDController(), drive::setOutput, drive);
+          return command;
+     }
+
+}
---
+++ Code_2021/Testing/First trajectory/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,85 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+
+
+public class Drivetrain extends SubsystemBase {
+     TalonSRX leftMaster = new TalonSRX(0);
+     TalonSRX rightMaster = new TalonSRX(2);
+
+     TalonSRX leftSlave = new TalonSRX(1);
+     TalonSRX rightSlave = new TalonSRX(3);
+
+     Encoder rightEncoder = new Encoder(0,1);
+     Encoder leftEncoder = new Encoder(2, 3);
+
+     AHRS gyro = new AHRS(SPI.Port.kMXP); //gyro object, check for specific gyro for correct
+
+     DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Units.inchesToMeters(28));
+     DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(kinematics, getHeading());
+
+     Pose2d pose; //stores position of the robot
+
+    SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0.268, 1.89, .243);  //NOT OUR VALUES, EXAMPLES
+
+    PIDController leftPIDController = new PIDController(9.95, 0 , 0);
+    PIDController rightPIDController = new PIDController(9.95, 0 , 0)
+
+
+ public Drivetrain(){
+     leftSlave.follow(leftMaster);
+     rightSlave.follow(rightMaster); //sets the slaves to follow the masters
+
+     rightMaster.setInverted(true); //sets right to invert
+     leftMaster.setInverted(false);
+ }
+
+ public Rotation2d getHeading(){
+     return Rotation2d.fromDegrees(-gyro.getAngle());
+ }
+
+ public DifferentialDriveWheelSpeeds getSpeeds(){
+     return new DifferentialDriveWheelSpeeds(  //returning wheel speeds in MetersPerSecond
+             rightEncoder.getRate() * 2 * Math.PI * Units.inchesToMeters(6) / 60,
+             leftEncoder.getRate()  * 2 * Math.PI * Units.inchesToMeters(6) / 60
+     );
+ }
+
+ public SimpleMotorFeedforward getFeedforward(){
+     return feedforward;
+ }
+ public PIDController getLeftPIDController(){
+        return leftPIDController;
+ }
+ public PIDController getRightPIDController(){
+        return rightPIDController;
+ }
+ public DifferentialDriveKinematics getKinematics(){
+        return kinematics;
+ }
+ public Pose2d getPose(){
+        return pose;
+ }
+ public void setOutput(double leftVolts, double rightVolts){
+        leftMaster.set(leftVolts / 12);
+        rightMaster.set(rightVolts / 12);
+ }
+    @Override
+    public void periodic() {
+        pose = odometry.update(getHeading(), getSpeeds());
+    }
+}
