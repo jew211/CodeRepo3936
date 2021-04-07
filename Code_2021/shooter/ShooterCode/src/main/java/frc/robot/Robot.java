bf10c08c1ee91a348b927456268678e21c853334
Ethan Green <Ethanrgreen2004@gmail.com>
2021-04-06 19:08:17 -0500

    changed tunnel to spark

--- Code_2021/shooter/ShooterCode/src/main/java/frc/robot/Robot.java
+++ Code_2021/shooter/ShooterCode/src/main/java/frc/robot/Robot.java
@@ -5,10 +5,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
+//import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
@@ -20,7 +22,7 @@ public class Robot extends TimedRobot {
   private final Talon m_rightMotor = new Talon(1);
   private final Talon R_intake = new Talon(2);
   private final Talon L_intake = new Talon(3);
-  private final Talon tunnel_Motor = new Talon(4);
+  private final Spark tunnel_Motor = new Spark(4);
   private final Talon shooter = new Talon(5);
 
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
@@ -38,7 +40,7 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(m_stick.getRawAxis(1), m_stick.getRawAxis(1));
+    m_robotDrive.arcadeDrive(m_stick.getRawAxis(1), m_stick.getRawAxis(4));
 
     R_intake.set(m_stick.getRawAxis(3));
     L_intake.set(m_stick.getRawAxis(3));
@@ -66,6 +68,8 @@ public class Robot extends TimedRobot {
       shooterspeed=1;
     }
     else shooterspeed=0;
+    SmartDashboard.putNumber("tunnel speed", tunnelspeed);
+    SmartDashboard.putNumber("shooter Speed", shooterspeed);
 
     tunnel_Motor.set(tunnelspeed);
     shooter.set(shooterspeed);
